41917ec0e6fb0d578d767c3de43a2e4a818b44d5
dangeli <dangeli@us.ibm.com>
2021-12-03 10:58:11 -0500

    day 2

---
+++ d2/p1.py
@@ -0,0 +1,34 @@
+
+def read_input():
+    lines = None
+    with open('input.txt') as f:
+    # with open('test.txt') as f:
+        lines = f.readlines()
+
+    if lines == None:
+        exit
+    else:
+        return lines   
+
+data = read_input()
+
+depth = 0
+distance = 0
+
+for i in data:
+    inst = i.split(" ")
+
+    if inst[0] == "forward":
+        distance = distance + int(inst[1])
+    elif inst[0] == "up":
+        depth = depth - int(inst[1])
+    else: #down
+        depth = depth + int(inst[1])
+
+    print(f"dist: {distance}, depth: {depth}")
+
+print(f"depth distance product: {depth * distance}")
+
+
+
+
